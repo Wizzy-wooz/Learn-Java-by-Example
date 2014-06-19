fa70e693e905689d3ccad45f26baa906d774ab6e
VodzianovaE <VodzianovaE@VodzianovaE-NT.BMS-CONSULTING.com>
2014-06-19 15:05:59 +0300

    New commit

--- src/javaapplication/IsPrime.java
+++ src/javaapplication/IsPrime.java
@@ -11,5 +11,10 @@ package javaapplication;
  * @author VodzianovaE
  */
 public class IsPrime {
-    
+    public static boolean isPrime(int N){
+    if (N<2) return false;
+    for(int i =2; i*i<=N; i++)
+        if(N%i==0) return false;
+    return true;
+    }
 }
